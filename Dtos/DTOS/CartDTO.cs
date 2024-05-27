82fd25dec83090e9b41c1758f72e7da718e4ff54
Denis <sossanaka000@gmail.com>
2024-05-28 00:34:21 +0500

    Update project: Refactoring and create documentation for Auth and Cart Controller

--- Dtos/DTOS/CartDTO.cs
+++ Dtos/DTOS/CartDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Elibri.DTOs.DTOS
 {
     public class CartDTO
     {
+        [Required]
         public int CartId { get; set; }
+        [Required]
         public string UserId { get; set; }
 
     }
 }
+//удаление товаров
